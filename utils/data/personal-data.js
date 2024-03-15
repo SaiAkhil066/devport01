2f29a47557e6da9518802445771d786c879f3ae7
N Sai Akhil <97727609+SaiAkhil066@users.noreply.github.com>
2024-03-15 16:12:44 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "N SAI AKHIL",
   profile: '/profile.png',
-  designation: "Software Developer",
+  designation: "Front-End Web Developer",
   description: "My name is N SAI AKHIL. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on Python & JavaScript and I love to do most of the things which is based on programming in Python and Web Dev using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'saiakhil066@gmail.com',
   phone: '+7989648797',
