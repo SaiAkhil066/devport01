2dbd3f95b4e5270756685804550fe4376cea3e91
N Sai Akhil <97727609+SaiAkhil066@users.noreply.github.com>
2024-03-15 16:15:53 +0530

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,26 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    title: "Post Graduation: MBA",
+    duration: "2023 - present",
+    institution: "Andhra University",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    title: "Bachelor Degree",
+    duration: "2020 - 2023",
+    institution: "Andhra University",
   },
   {
     id: 3,
+    title: "Higher Secondary Certificate",
+    duration: "2015 - 2017",
+    institution: "Noakhali Islamia Kamil Madrasah",
+  },
+  {
+    id: 4,
     title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    duration: "2005 - 2015",
+    institution: "Sri Chaitanya Techno School",
   }
 ]
